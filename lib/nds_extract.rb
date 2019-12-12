50674b436b7ccc9cc44f1e29d01e84cf30213cd4
vien pham <vienpham2019@gmail.com>
2019-12-12 01:41:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -27,10 +27,9 @@ end
 def list_of_directors(source)
   # Write this implementation
   director_name = {}
-  array = directors_totals(source).to_a
   index = 0
-  while index < array.length do
-    director_name << array[index][0]
+  while index < source.length do
+    director_name << source[index][:name]
     index += 1
   end
   
